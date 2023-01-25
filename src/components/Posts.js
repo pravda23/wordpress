92e7f06cc5c1d8a7a26019e7b85684b8cd0692eb
pravda23 <pravda23@gmail.com>
2023-01-25 10:55:36 +0200

    first commit

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,8 @@
-import { useState, useEffect } from "react";
-import Post from "./Post";
-
 const Posts = (data) => {
   console.log(data.data);
   {
     data.data.map((i) => {
-      <h1>{i.id}</h1>;
+      return <h1>{i.id}</h1>;
     });
   }
 };
